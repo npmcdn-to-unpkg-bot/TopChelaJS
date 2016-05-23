098ed456426b5481f2bb0bb075e99402f5dd28f6
Paul Arizpe <kiramishima@outlook.com>
2016-05-22 23:55:35 -0500

    Add Testing

--- app/shared/config.js
+++ app/shared/config.js
@@ -3,7 +3,7 @@ var Config = (function () {
     function Config() {
     }
     Config.apiUrl = "https://api.nytimes.com/svc/topstories/v2/world.json";
-    Config.token = "cf5eef9392174ca59acd64f8877f3009";
+    Config.token = "";
     return Config;
 }());
 exports.Config = Config;
